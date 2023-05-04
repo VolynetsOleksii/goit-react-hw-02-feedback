18fd19a270069e6e106a62b94a109a00b80172db
VolynetsOleksii <volinetsaleksey@gmail.com>
2023-05-04 22:26:53 +0300

    add steps 2 and 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,34 +3,52 @@ import { StatisticList } from './StatisticList/StatisticList';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 export class App extends Component {
-
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
-  onSetFeedback = (option) => {
-    this.setState((prevState) => {
+  onSetFeedback = option => {
+    this.setState(prevState => {
       return {
         [option]: prevState[option] + 1,
       };
     });
   };
 
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositivePercentage = () => {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100) || 0;
+  };
+
   render() {
     const keys = Object.keys(this.state);
     const { good, neutral, bad } = this.state;
+
     return (
       <div>
         <Section title="Please leave feedback">
-          <FeedbackOptions options={keys} onLeaveFeedback={this.onSetFeedback}></FeedbackOptions>
+          <FeedbackOptions
+            options={keys}
+            onLeaveFeedback={this.onSetFeedback}
+          ></FeedbackOptions>
         </Section>
         <Section title="Statistics">
-          <StatisticList good={good} neutral={neutral} bad={bad} />
+          <StatisticList
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositivePercentage()}
+          />
         </Section>
       </div>
     );
   }
 }
-
--- src/components/StatisticList/StatisticList.jsx
+++ src/components/StatisticList/StatisticList.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 
-export const StatisticList = ({ good, neutral, bad }) => {
+export const StatisticList = ({ good, neutral, bad, total, positivePercentage}) => {
   return (
     <ul>
       <li>Good: {good}</li>
       <li>Neutral: {neutral}</li>
       <li>Bad: {bad}</li>
+      <li>Total: {total}</li>
+      <li>Positive feedback: {positivePercentage}%</li>
     </ul>
   );
 };
@@ -14,4 +16,6 @@ StatisticList.propTypes = {
   good: PropTypes.number.isRequired,
   neutral: PropTypes.number.isRequired,
   bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
